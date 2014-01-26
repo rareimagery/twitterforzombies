bb083767363ba28638101460b2609305c9d4c47b
rareimagery <rareblue@gmail.com>
2014-01-26 10:53:09 -0700

    twitter for zombies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterForZombies::Application.config.secret_key_base = '5b41fc39bf7a12fc6784aea58460ce4c05c971b6c03ea90f7fe2721a86de8e5b15f79b0cb50671351c44ffe590971d1ad48fd064de1e3a89e7098ed1aa5b68a1'
